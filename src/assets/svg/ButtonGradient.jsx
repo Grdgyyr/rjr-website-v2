6413da2c13770a9c1783fa4dd64cd6c3f939c333
ron <ronmagpantay1@gmail.com>
2025-03-07 11:43:59 +0800

    style: update button gradients and color scheme in SVG and Tailwind config

--- src/assets/svg/ButtonGradient.jsx
+++ src/assets/svg/ButtonGradient.jsx
@@ -2,27 +2,28 @@ const ButtonGradient = () => {
   return (
     <svg className="block" width={0} height={0}>
       <defs>
+        {/* Left Gradient */}
         <linearGradient id="btn-left" x1="50%" x2="50%" y1="0%" y2="100%">
-          <stop offset="0%" stopColor="#89F9E8" />
-          <stop offset="100%" stopColor="#FACB7B" />
+          <stop offset="0%" stopColor="#B20202" />
+          <stop offset="100%" stopColor="#890405" />
         </linearGradient>
+
+        {/* Top Gradient */}
         <linearGradient id="btn-top" x1="100%" x2="0%" y1="50%" y2="50%">
-          <stop offset="0%" stopColor="#D87CEE" />
-          <stop offset="100%" stopColor="#FACB7B" />
+          <stop offset="0%" stopColor="#B20202" />
+          <stop offset="100%" stopColor="#890405" />
         </linearGradient>
+
+        {/* Bottom Gradient */}
         <linearGradient id="btn-bottom" x1="100%" x2="0%" y1="50%" y2="50%">
-          <stop offset="0%" stopColor="#9099FC" />
-          <stop offset="100%" stopColor="#89F9E8" />
+          <stop offset="0%" stopColor="#890405" />
+          <stop offset="100%" stopColor="#B20202" />
         </linearGradient>
-        <linearGradient
-          id="btn-right"
-          x1="14.635%"
-          x2="14.635%"
-          y1="0%"
-          y2="100%"
-        >
-          <stop offset="0%" stopColor="#9099FC" />
-          <stop offset="100%" stopColor="#D87CEE" />
+
+        {/* Right Gradient */}
+        <linearGradient id="btn-right" x1="14.635%" x2="14.635%" y1="0%" y2="100%">
+          <stop offset="0%" stopColor="#890405" />
+          <stop offset="100%" stopColor="#B20202" />
         </linearGradient>
       </defs>
     </svg>
