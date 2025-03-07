fa39d0787ef026457dc097cdedba163f2f04ec74
ron <ronmagpantay1@gmail.com>
2025-03-07 12:10:42 +0800

    refactor: remove unused social media icons from assets and constants

--- src/assets/index.js
+++ src/assets/index.js
@@ -58,12 +58,6 @@ import benefitIcon3 from "./benefits/icon-3.svg";
 import benefitIcon4 from "./benefits/icon-4.svg";
 import benefitImage2 from "./benefits/image-2.png";
 
-import discordBlack from "./socials/discord.svg";
-import facebook from "./socials/facebook.svg";
-import instagram from "./socials/instagram.svg";
-import telegram from "./socials/telegram.svg";
-import twitter from "./socials/twitter.svg";
-import { roadmap } from "../constants";
 
 export {
   brainwave,
