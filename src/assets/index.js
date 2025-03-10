511dfa2594aa881bc78a9188b91c0bf5d1226a22
plip <johnphilip.ravena@gmail.com>
2025-03-10 15:40:08 +0800

    feat: add new benefit images and update imports in Benefits component

--- src/assets/index.js
+++ src/assets/index.js
@@ -35,7 +35,7 @@ import office from "./collaboration/office.png";
 
 
 import lines from "./pricing/lines.svg";
-import stars from "./pricing/stars.svg";
+import stars from "./pricing/stars.png";
 
 
 import roadmap5 from "./roadmap/electronics.png";
@@ -46,18 +46,19 @@ import roadmap9 from "./roadmap/medicalsupplies.png";
 import roadmap10 from "./roadmap/equipments.png";
 
 
-import benefitCard1 from "./benefits/card-1.svg";
+import benefitCard1 from "./benefits/card-1.png";
 import benefitCard2 from "./benefits/card-2.svg";
 import benefitCard3 from "./benefits/card-3.svg";
-import benefitCard4 from "./benefits/card-4.svg";
-import benefitCard5 from "./benefits/card-5.svg";
-import benefitCard6 from "./benefits/card-6.svg";
 import benefitIcon1 from "./benefits/icon-1.svg";
 import benefitIcon2 from "./benefits/icon-2.svg";
 import benefitIcon3 from "./benefits/icon-3.svg";
 import benefitIcon4 from "./benefits/icon-4.svg";
-import benefitImage2 from "./benefits/image-2.png";
-
+import benefitImage1 from "./benefits/image-1.svg";
+import benefitImage2 from "./benefits/image-2.svg";
+import benefitImage3 from "./benefits/image-3.svg";
+import benefitImage4 from "./benefits/image-4.svg";
+import benefitImage5 from "./benefits/image-5.svg";
+import benefitImage6 from "./benefits/image-6.svg";
 
 export {
   brainwave,
@@ -103,12 +104,14 @@ export {
   benefitCard1,
   benefitCard2,
   benefitCard3,
-  benefitCard4,
-  benefitCard5,
-  benefitCard6,
   benefitIcon1,
   benefitIcon2,
   benefitIcon3,
   benefitIcon4,
+  benefitImage1,
   benefitImage2,
+  benefitImage3,
+  benefitImage4,
+  benefitImage5,
+  benefitImage6,
 };
--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -11,7 +11,7 @@ const Benefits = () => {
       <div className="container relative z-2">
         <Heading
           className="md:max-w-md lg:max-w-2xl"
-          title="Chat Smarter, Not Harder with Brainwave"
+          title="Products and Services"
         />
 
         <div className="flex flex-wrap gap-10 mb-10">
@@ -23,20 +23,10 @@ const Benefits = () => {
               }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[3rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
                 <div className="flex items-center mt-auto">
-                  <img
-                    src={item.iconUrl}
-                    width={48}
-                    height={48}
-                    alt={item.title}
-                  />
-                  <p className="ml-auto font-code text-xs font-bold text-n-1 uppercase tracking-wider">
-                    Explore more
-                  </p>
-                  <Arrow />
                 </div>
               </div>
 
@@ -46,7 +36,7 @@ const Benefits = () => {
                 className="absolute inset-0.5 bg-n-8"
                 style={{ clipPath: "url(#benefits)" }}
               >
-                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-10">
+                <div className="absolute inset-0 opacity-30 transition-opacity, transform: hover:opacity-10">
                   {item.imageUrl && (
                     <img
                       src={item.imageUrl}
