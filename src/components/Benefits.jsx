ad8f93821ff98e5efdc8e31e2d76cf74e740c954
ron <ronmagpantay1@gmail.com>
2025-03-11 12:39:48 +0800

    style: adjust padding and opacity in products component

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -23,11 +23,10 @@ const Benefits = () => {
               }}
               key={item.id}
             >
-              <div className="relative z-2 flex flex-col min-h-[22rem] p-[3rem] pointer-events-none">
+              <div className="relative z-2 flex flex-col min-h-[22rem] p-[2.4rem] pointer-events-none">
                 <h5 className="h5 mb-5">{item.title}</h5>
                 <p className="body-2 mb-6 text-n-3">{item.text}</p>
-                <div className="flex items-center mt-auto">
-                </div>
+                
               </div>
 
               {item.light && <GradientLight />}
@@ -36,7 +35,7 @@ const Benefits = () => {
                 className="absolute inset-0.5 bg-n-8"
                 style={{ clipPath: "url(#benefits)" }}
               >
-                <div className="absolute inset-0 opacity-30 transition-opacity, transform: hover:opacity-10">
+                <div className="absolute inset-0 opacity-0 transition-opacity hover:opacity-25">
                   {item.imageUrl && (
                     <img
                       src={item.imageUrl}
