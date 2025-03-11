4c58677c2209e1ffb32216c6734fa1b0d4fecd60
ron <ronmagpantay1@gmail.com>
2025-03-11 14:30:43 +0800

    refactor: remove unused SVG assets and imports from components

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,24 +1,9 @@
 import rjrlogo from "./rjrlogo.png";
-import brainwave from "./brainwave.svg";
 import check from "./check.svg";
-import brainwaveSymbol from "./brainwave-symbol.svg";
-import brainwaveWhiteSymbol from "./brainwave-symbol-white.svg";
-import play from "./play.svg";
 import gradient from "./gradient.png";
-import smallSphere from "./4-small.png";
 import grid from "./grid.png";
 import check2 from "./check-02.svg";
 import loading1 from "./loading-01.svg";
-import yourlogo from "./yourlogo.svg";
-import homeSmile from "./home-smile.svg";
-import file02 from "./file-02.svg";
-import searchMd from "./search-md.svg";
-import plusSquare from "./plus-square.svg";
-import recording03 from "./recording-03.svg";
-import recording01 from "./recording-01.svg";
-import disc02 from "./disc-02.svg";
-import chromecast from "./chrome-cast.svg";
-import sliders04 from "./sliders-04.svg";
 import loading from "./loading.png";
 
 
@@ -62,26 +47,11 @@ import benefitImage6 from "./benefits/image-6.svg";
 
 export {
   rjrlogo,
-  brainwave,
   check,
   check2,
   loading1,
-  brainwaveSymbol,
-  brainwaveWhiteSymbol,
-  play,
   gradient,
-  smallSphere,
   grid,
-  yourlogo,
-  homeSmile,
-  file02,
-  searchMd,
-  plusSquare,
-  recording03,
-  recording01,
-  disc02,
-  chromecast,
-  sliders04,
   loading,
   curve1,
   curve2,
