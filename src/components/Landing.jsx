25e94aa2f0ed15f0dbd4ecf04dcfcaadcc5d38d0
ron <ronmagpantay1@gmail.com>
2025-03-17 14:28:28 +0800

    refactor: update Footer and Landing components for improved layout and accessibility

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,14 +6,14 @@ const Landing = () => {
   return (
     <Section className="overflow-hidden" id="landing">
       <div className="container relative z-2">
-        <div className="hidden relative justify-center mb-[6.5rem] lg:flex">
+        <div className="relative justify-center mb-[6.5rem] lg:flex">
           <img
             src={rjrlogo}
-            className="relative z-1"
+            className="relative z-1 mx-auto lg:mx-0"
             width={400}
             height={400}
           />
-          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none">
+          <div className="absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none hidden lg:block">
             <img
               src={stars}
               className="w-full"
@@ -32,9 +32,7 @@ const Landing = () => {
         <div className="flex justify-center mt-10">
           <a
             className="text-xs font-code font-bold tracking-wider uppercase border-b"
-            href="https://www.facebook.com/RJRMerchandising"
-            target="_blank"
-            rel="noopener noreferrer"
+            href="#profile"
           >
             See the full details
           </a>
